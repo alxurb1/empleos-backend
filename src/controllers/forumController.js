9880490dc0460fc5e3cf4d8e15f005fa675f69ca
David <josedavidcon22@gmail.com>
2026-04-10 00:41:54 -0600

    crud completo de foro y filtro por categoria

---
+++ src/controllers/forumController.js
@@ -0,0 +1,46 @@
+import * as forumService from '../services/forumService.js';
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const { category } = req.query; 
+    
+    const result = await forumService.getPosts(category);
+    res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const createPost = async (req, res, next) => {
+  try {
+    const result = await forumService.createPost(req.body);
+    
+    res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  try {
+    const { id } = req.params; 
+    
+    const result = await forumService.updatePost(id, req.body);
+    
+    res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deletePost = async (req, res, next) => {
+  try {
+    const { id } = req.params; 
+    
+    const result = await forumService.deletePost(id);
+    
+    res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import vacancyRoutes from "./routes/vacancyRoutes.js";
 import applicationRoutes from "./routes/applicationRoutes.js";
 import resourcesRoutes from "./routes/resourcesRoutes.js";
+import forumRoutes from './routes/forumRoutes.js';
 
 import dotenv from "dotenv";
 
@@ -27,6 +28,7 @@ app.use("/register", authRoutes);
 app.use("/vacancy", vacancyRoutes);
 app.use("/", applicationRoutes);
 app.use("/resources", resourcesRoutes);
+app.use('/forum/posts', forumRoutes);
 
 app.use(errorHandler);
 
